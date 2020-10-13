87ba97ce8dd8cba0afe5b638075024f70f758bda
Anurag Raina <rainakalhan90@gmail.com>
2020-10-14 02:36:34 +0530

    Update 'About Me' section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,17 +5,7 @@ class About extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			skills: [
-				{ id: 'HTML5_skill', content: 'HTML5', porcentage: '80%', value: '80' },
-				{ id: 'CSS3_skill', content: 'CSS3', porcentage: '75%', value: '75' },
-				{ id: 'JavaScript_skill', content: 'JavaScript', porcentage: '90%', value: '90' },
-				{ id: 'PHP_skill', content: 'PHP', porcentage: '70%', value: '70' },
-				{ id: 'ReactJS_skill', content: 'ReactJS', porcentage: '80%', value: '80' },
-				{ id: 'Python_skill', content: 'Python', porcentage: '75%', value: '75' },
-				{ id: 'VanillaJS_skill', content: 'VanillaJS', porcentage: '85%', value: '85' },
-				{ id: 'Wordpress_skill', content: 'Wordpress', porcentage: '80%', value: '80' },
-			],
-			about_me: [
+			aboutMe: [
 				{
 					id: 'first-p-about',
 					content: 'I am a Computer Science undergrad graduating in 2021.',
@@ -23,12 +13,12 @@ class About extends React.Component {
 				{
 					id: 'second-p-about',
 					content:
-						'With over two years of experience developing web applications using the latest front-end and back-end technologies.',
+						'With over an year of experience developing web applications using the latest front-end and back-end technologies, I am currently undergoing training for Mobile App Developement using Flutter.',
 				},
 				{
 					id: 'third-p-about',
 					content:
-						'Motivated designer and developer with experience creating custom websites with ReactJs, JavaScript, HTML5, and CSS3. Strong collaboration skills and proven history of application development. Wordpress and MySQL.',
+						'Motivated developer with experience in developing custom web-apps specializing in MERN stack, I also have some experience working with Django among other tools and technologies.',
 				},
 			],
 		};
@@ -43,24 +33,18 @@ class About extends React.Component {
 							<div className='box-shadow-full'>
 								<div className='row'>
 									<div className='col-md-4 image-div'>
-										<img
-											src={myImage}
-											className='img-fluid rounded b-shadow-a'
-											alt='Profile'
-										/>
+										<img src={myImage} className='img-fluid rounded b-shadow-a' alt='Profile' />
 									</div>
 									<div className='col-md-8'>
 										<div className='about-me pt-4 pt-md-0'>
 											<div className='title-box-2'>
 												<h5 className='title-left'>About Me</h5>
 											</div>
-											{this.state.about_me.map(content => {
-												return (
-													<p className='lead' key={content.id}>
-														{content.content}
-													</p>
-												);
-											})}
+											{this.state.aboutMe.map(content => (
+												<p className='lead' key={content.id}>
+													{content.content}
+												</p>
+											))}
 										</div>
 									</div>
 								</div>
